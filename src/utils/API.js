e59a3195dc90a2dd7fec3fd5c79e08df010379ec
Lizzie Hurley <l.hurley58@gmail.com>
2017-11-28 20:08:33 -0600

    api.js update

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,13 +3,10 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
-  }
-};
+  getWineList: function() {
+      return axios.get("https://slosh.io/routes/api/alcohol");
+    },
+    getFood: function() {
+      return axios.get("https://slosh.io/routes/api/alcohol/" +pairings "/" );
+    }
+    };

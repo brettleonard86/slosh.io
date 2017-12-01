05d7381aaf8fb675f93d68834768fad5f7fce411
Josh <j8shigarcia@aol.com>
2017-11-30 22:00:02 -0600

    sharing backend problems

--- routes/api/alcohol.js
+++ routes/api/alcohol.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const alcoholController = require("../../controllers/alcoholController");
 
 // Matches with "/api/alcohol + "my pairing"
-router.route("/:pairings")
-  .get(alcoholController.findAllPairings);
+//router.route("/:pairings")
+  //.get(alcoholController.findAllPairings);
 
 // Matches with "/api/alcohol"
 router.route("/")

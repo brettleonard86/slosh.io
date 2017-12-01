3906e6280429d4ff9ace6840ec748cc20f16806a
Brett Leonard <brettleonard86@gmail.com>
2017-11-30 20:24:47 -0600

    user.js

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -9,6 +9,8 @@ class UserForm extends React.Component {
     this.state = {
       choice: 'Stew',
       isModalOpen: false
+      // userName: "",
+      // userEmail: ""
     };
   }
   onChange = (e) => {
@@ -36,12 +38,20 @@ class UserForm extends React.Component {
    }
 
    responseGoogle(response) {
+    var userName = response.w3.ig;
+    var userEmail =response.w3.U3;
+    var id_token = response.getAuthResponse().id_token;
+    var user = {
+      name: userName,
+      email: userEmail
+    }
+    console.log({accessToken: id_token});
     console.log(response);
-    console.log("The user's name is " + response.w3.ig);
-    console.log("The user email is " + response.w3.U3);
+    console.log("The user's name is " + userName);
+    console.log("The user email is " + userEmail);
    }
 
-   
+
   render() {
     const { choice } = this.state;
     return (
@@ -92,3 +102,39 @@ class UserForm extends React.Component {
 }
 
 export default UserForm;
+
+
+
+// function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   // console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//   console.log('Name: ' + profile.getName());
+//   console.log('Image URL: ' + profile.getImageUrl());
+//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
+
+// function onFailure(error) {
+//   console.log(error);
+// }
+
+// function renderButton() {
+//   gapi.signin2.render('my-signin2', {
+//     'scope': 'profile email',
+//     'width': 240,
+//     'height': 50,
+//     'longtitle': true,
+//     'theme': 'dark',
+//     'onsuccess': onSuccess,
+//     'onfailure': onFailure
+//   });
+// }
+
+
+// Sign out code from Google.
+// function signOut() {
+//   var auth2 = gapi.auth2.getAuthInstance();
+//   auth2.signOut().then(function () {
+//     console.log('User signed out.');
+//   });
+// }
